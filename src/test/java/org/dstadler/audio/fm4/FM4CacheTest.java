39441c07eef623e069ba40ff6b504e52def6507a
Dominik Stadler <dominik.stadler@gmx.at>
2020-04-14 16:20:13 +0200

    Improve assertion information

--- src/test/java/org/dstadler/audio/fm4/FM4CacheTest.java
+++ src/test/java/org/dstadler/audio/fm4/FM4CacheTest.java
@@ -8,14 +8,14 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 public class FM4CacheTest {
-
     @Test
     public void testCache() {
         try (FM4Cache cache = new FM4Cache()) {
             assertEquals(0, cache.size());
 
             cache.refresh();
-            assertTrue(cache.size() > 0);
+            assertTrue("Cache should not be empty now, check stdout for error messages while fetching the data",
+                    cache.size() > 0);
 
             assertNotNull(cache.get("4UL"));
             assertNull(cache.get("NOT EXISTING"));
