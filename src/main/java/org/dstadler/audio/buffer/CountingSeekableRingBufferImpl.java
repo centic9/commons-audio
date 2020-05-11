3ef9c73a2a047e8611acfd3ea950b952e1006809
Dominik Stadler <dominik.stadler@gmx.at>
2020-05-11 09:42:52 +0200

    Add CountingSeekableRingBufferImpl

---
+++ src/main/java/org/dstadler/audio/buffer/CountingSeekableRingBufferImpl.java
@@ -0,0 +1,231 @@
+package org.dstadler.audio.buffer;
+
+import org.dstadler.commons.metrics.MovingAverage;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Wrapper-interface around a {@link SeekableRingBuffer} which computes some
+ * statistics and counts written and read chunks and bytes as well
+ * as a moving average of how long it took on average to receive chunks
+ * of data.
+ *
+ * This average is used to estimate how long it takes when the audio
+ * data is played at normal speed.
+ */
+public class CountingSeekableRingBufferImpl implements CountingSeekableRingBuffer {
+    private final SeekableRingBuffer<Chunk> delegate;
+    private final long start = System.currentTimeMillis();
+
+    protected static final double DEFAULT_CHUNKS_PER_SECOND = 1.5;
+
+    private AtomicLong bytesWrittenOverall = new AtomicLong();
+    private AtomicLong chunksWrittenOverall = new AtomicLong();
+
+    private AtomicLong bytesReadOverall = new AtomicLong();
+    private AtomicLong chunksReadOverall = new AtomicLong();
+
+    // record timestamps of the last 300 chunks so we can compute how many we do per second
+    private static final int MOVING_WINDOW = 300;
+    private final MovingAverage chunksWrittenPerSecond = new MovingAverage(MOVING_WINDOW);
+    private final MovingAverage chunksReadPerSecond = new MovingAverage(MOVING_WINDOW);
+
+    public CountingSeekableRingBufferImpl(SeekableRingBuffer<Chunk> delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public void add(Chunk chunk) {
+        delegate.add(chunk);
+
+        bytesWrittenOverall.addAndGet(chunk.size());
+        chunksWrittenOverall.addAndGet(1);
+
+        // compute how long it takes until we add a certain number of chunks
+        // to allow computation of a moving average written chunks per second
+        synchronized (chunksWrittenPerSecond) {
+            chunksWrittenPerSecond.add(chunk.getTimestamp());
+        }
+    }
+
+    /**
+     * Used to add chunks which do not count as normal
+     * traffic, e.g. when pre-filling or when bulk-adding
+     * content.
+     *
+     * @param chunk A chunk of bytes to store
+     */
+    @Override
+    public void addNoStats(Chunk chunk) {
+        delegate.add(chunk);
+    }
+
+    @Override
+    public Chunk next() {
+        Chunk chunk = delegate.next();
+        if(chunk == null) {
+            return null;
+        }
+
+        bytesReadOverall.addAndGet(chunk.size());
+        chunksReadOverall.addAndGet(1);
+
+        // compute how long it takes between reads
+        synchronized (chunksReadPerSecond) {
+            chunksReadPerSecond.add(System.currentTimeMillis());
+        }
+
+        return chunk;
+    }
+
+    @Override
+    public Chunk peek() {
+        return delegate.peek();
+    }
+
+    @Override
+    public int seek(int nrOfChunks) {
+        return delegate.seek(nrOfChunks);
+    }
+
+    @Override
+    public boolean empty() {
+        return delegate.empty();
+    }
+
+    @Override
+    public boolean full() {
+        return delegate.full();
+    }
+
+    @Override
+    public int capacity() {
+        return delegate.capacity();
+    }
+
+    @Override
+    public int size() {
+        return delegate.size();
+    }
+
+    @Override
+    public int fill() {
+        return delegate.fill();
+    }
+
+    @Override
+    public void reset() {
+        delegate.reset();
+    }
+
+    @Override
+    public int bufferedForward() {
+        return delegate.bufferedForward();
+    }
+
+    @Override
+    public int bufferedBackward() {
+        return delegate.bufferedBackward();
+    }
+
+    @Override
+    public void close() {
+        delegate.close();
+    }
+
+    @Override
+    public double getChunksWrittenPerSecond() {
+        synchronized (chunksWrittenPerSecond) {
+            return computeChunksPerSecond(chunksWrittenPerSecond);
+        }
+    }
+
+    @Override
+    public double getChunksReadPerSecond() {
+        synchronized (chunksReadPerSecond) {
+            return computeChunksPerSecond(chunksReadPerSecond);
+        }
+    }
+
+    @Override
+    public double getChunksPerSecond() {
+        double written = getChunksWrittenPerSecond();
+        double read = getChunksReadPerSecond();
+
+        // having less than half a chunk per second is an indication
+        // that the values are off for some reason, e.g.
+        // having a download and not a live stream
+        if((written < 0.5 || written > 5) &&
+                (read < 0.5 || read > 5)) {
+            return DEFAULT_CHUNKS_PER_SECOND;
+        }
+
+        if(written < 0.5 || written > 5) {
+            return read;
+        }
+
+        if(read < 0.5 || read > 5) {
+            return written;
+        }
+
+        return Math.min(written, read);
+    }
+
+    private static double computeChunksPerSecond(MovingAverage chunksPerSecond) {
+        if(chunksPerSecond.getFill() == 0) {
+            return 0;
+        }
+
+        // compute the duration of receiving all the chunks
+        // in the sliding window, but exclude time-jumps, e.g
+        // where the Laptop was in Sleep/Hibernate
+        long durationAdjust = 0;
+        long countAdjust = 0;
+        long[] window = chunksPerSecond.getWindow();
+        long prevValue = window[0];
+        for (long value : window) {
+            // only count values up to 5 seconds, larger
+            // times between chunks are very likely stopped
+            // or sleeping applications
+            if(value - prevValue >= 5000) {
+                durationAdjust += (value - prevValue);
+                countAdjust++;
+            }
+            prevValue = value;
+        }
+
+        long start = chunksPerSecond.getFirst();
+        long end = chunksPerSecond.getLast();
+        double durationInSec = ((double) end - start - durationAdjust)/1000;
+
+        double chunksInWindow = chunksPerSecond.getFill() - countAdjust;
+
+        // we have up to 300 items in the moving average,
+        // we get per-second rate by dividing by the number
+        // of seconds it took to receive that many
+        return chunksInWindow / durationInSec;
+    }
+
+    // for testing
+    public long getChunksReadOverall() {
+        return chunksReadOverall.get();
+    }
+
+    private double getPerSecond(AtomicLong bytesWrittenOverall) {
+        long time = System.currentTimeMillis() - start;
+        time = time / 1000;
+        return ((double) bytesWrittenOverall.get()) / time;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Written: " + bytesWrittenOverall.get() + " bytes/" + chunksWrittenOverall.get() + " chunks" +
+                        ", " + String.format("%.2f", getPerSecond(bytesWrittenOverall)) + " bytes/s/" + String.format("%.2f", getPerSecond(chunksWrittenOverall)) + " chunks/s" +
+
+                        ", Read: " + bytesReadOverall.get() + " bytes/" + chunksReadOverall.get() + " chunks" +
+                        ", " + String.format("%.2f", getPerSecond(bytesReadOverall)) + " bytes/s/" + String.format("%.2f", getPerSecond(chunksReadOverall)) + " chunks/s" +
+
+                        ", " + delegate.toString();
+    }
+}
---
+++ src/test/java/org/dstadler/audio/buffer/CountingSeekableRingBufferImplTest.java
@@ -0,0 +1,146 @@
+package org.dstadler.audio.buffer;
+
+import org.dstadler.commons.testing.TestHelpers;
+import org.dstadler.commons.testing.ThreadTestHelper;
+import org.junit.Test;
+
+import static org.dstadler.audio.buffer.CountingSeekableRingBufferImpl.DEFAULT_CHUNKS_PER_SECOND;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class CountingSeekableRingBufferImplTest extends AbstractBlockingSeekableRingBufferTester {
+    private static final int NUMBER_OF_THREADS = 10;
+    private static final int NUMBER_OF_TESTS = 10000;
+
+    // reuse tests from the abstract tester here as well to ensure that the Counting implementation
+    // delegates all calls correctly
+    @Override
+    SeekableRingBuffer<Chunk> getBlockingSeekableRingBuffer() {
+        return new CountingSeekableRingBufferImpl(new BlockingSeekableRingBuffer(10));
+    }
+
+    private CountingSeekableRingBuffer getBuffer() {
+        return (CountingSeekableRingBuffer)buffer;
+    }
+
+    @Test
+    public void testCounting() {
+        assertEquals(0, getBuffer().getChunksWrittenPerSecond(), 0);
+        assertEquals(0, getBuffer().getChunksReadPerSecond(), 0);
+        assertEquals(DEFAULT_CHUNKS_PER_SECOND, getBuffer().getChunksPerSecond(), 0.01);
+
+        getBuffer().addNoStats(new Chunk(new byte[0], "", 0));
+        assertEquals(0, getBuffer().getChunksWrittenPerSecond(), 0);
+        assertEquals(0, getBuffer().getChunksReadPerSecond(), 0);
+        assertEquals(DEFAULT_CHUNKS_PER_SECOND, getBuffer().getChunksPerSecond(), 0.01);
+
+        buffer.add(new Chunk(new byte[0], "", 0));
+        assertTrue(Double.isInfinite(getBuffer().getChunksWrittenPerSecond()));
+        assertEquals(0, getBuffer().getChunksReadPerSecond(), 0);
+        assertEquals(DEFAULT_CHUNKS_PER_SECOND, getBuffer().getChunksPerSecond(), 0.01);
+    }
+
+    @Test
+    public void testGetChunksWrittenPerSec() {
+        assertEquals(0, getBuffer().getChunksWrittenPerSecond(), 0);
+
+        getBuffer().add(new Chunk(new byte[] { 1 }, "", System.currentTimeMillis()-6000));
+        getBuffer().add(new Chunk(new byte[] { 1 }, "", System.currentTimeMillis()-2000));
+        getBuffer().add(new Chunk(new byte[] { 1 }, "", System.currentTimeMillis()-1000));
+
+        assertEquals(0.6, getBuffer().getChunksWrittenPerSecond(), 0.01);
+        assertEquals(0, getBuffer().getChunksReadPerSecond(), 0);
+        assertEquals(0.6, getBuffer().getChunksPerSecond(), 0.01);
+    }
+
+    @Test
+    public void testGetChunksReadPerSec() throws InterruptedException {
+        assertEquals(0, getBuffer().getChunksWrittenPerSecond(), 0);
+
+        long tms = System.currentTimeMillis() - 6000;
+        getBuffer().add(new Chunk(new byte[] { 1 }, "", tms));
+        getBuffer().add(new Chunk(new byte[] { 1 }, "", tms));
+        getBuffer().add(new Chunk(new byte[] { 1 }, "", tms));
+        getBuffer().add(new Chunk(new byte[] { 1 }, "", tms));
+        getBuffer().add(new Chunk(new byte[] { 1 }, "", tms));
+
+        long start = System.currentTimeMillis();
+
+        final double writtenPerSecond = getBuffer().getChunksWrittenPerSecond();
+        assertTrue("Had: " + writtenPerSecond, Double.isInfinite(writtenPerSecond));
+        assertEquals(0, getBuffer().getChunksReadPerSecond(), 0);
+        assertEquals(DEFAULT_CHUNKS_PER_SECOND, getBuffer().getChunksPerSecond(), 0.01);
+
+        getBuffer().next();
+        Thread.sleep(600);
+        getBuffer().next();
+        Thread.sleep(200);
+        getBuffer().next();
+        Thread.sleep(100);
+        getBuffer().next();
+
+        long end = System.currentTimeMillis();
+
+        assertTrue(Double.isInfinite(getBuffer().getChunksWrittenPerSecond()));
+        final double expected = 4 / ((double) (end - start)) * 1000;
+        assertEquals(expected, getBuffer().getChunksReadPerSecond(), 0.01);
+        assertEquals(expected, getBuffer().getChunksPerSecond(), 0.01);
+    }
+
+    @Test
+    public void testGetChunksWrittenPerSecMany() {
+        // add two chunks per second
+        for(int i = 0; i < 100;i++) {
+            buffer.add(new Chunk(new byte[0], "", 500*i));
+        }
+        assertEquals(2.0202, getBuffer().getChunksWrittenPerSecond(), 0.01);
+        assertEquals(0, getBuffer().getChunksReadPerSecond(), 0);
+        assertEquals(2.0202, getBuffer().getChunksPerSecond(), 0.01);
+    }
+
+    @Test
+    public void testGetChunksWrittenPerSecTimeJump() {
+        assertEquals(0, getBuffer().getChunksWrittenPerSecond(), 0);
+
+        long now = System.currentTimeMillis();
+        getBuffer().add(new Chunk(new byte[] { 1 }, "", now - 64000));
+        getBuffer().add(new Chunk(new byte[] { 1 }, "", now - 62000));
+        getBuffer().add(new Chunk(new byte[] { 1 }, "", now - 2000));
+        getBuffer().add(new Chunk(new byte[] { 1 }, "", now - 1000));
+
+        assertEquals(1, getBuffer().getChunksWrittenPerSecond(), 0);
+        assertEquals(0, getBuffer().getChunksReadPerSecond(), 0);
+        assertEquals(1, getBuffer().getChunksPerSecond(), 0.01);
+    }
+
+    @Override
+    @Test
+    public void testToString() {
+        TestHelpers.ToStringTest(buffer);
+
+        getBuffer().addNoStats(new Chunk(new byte[0], "", 0));
+        TestHelpers.ToStringTest(buffer);
+    }
+
+    @Test
+    public void testThreaded() throws Throwable {
+        ThreadTestHelper helper =
+                new ThreadTestHelper(NUMBER_OF_THREADS, NUMBER_OF_TESTS);
+
+        helper.executeTest(new ThreadTestHelper.TestRunnable() {
+            @Override
+            public void doEnd(int threadNum) {
+                buffer.close();
+            }
+
+            @Override
+            public void run(int threadNum, int iter) {
+                if (iter % 2 == 0) {
+                    assertTrue(getBuffer().getChunksWrittenPerSecond() >= 0);
+                } else {
+                    buffer.add(new Chunk(new byte[] {1,2,3}, "bla bla", 0));
+                }
+            }
+        });
+    }
+}
