2d44dd336b7ee5342af593a87a4cd66051157574
Dominik Stadler <dominik.stadler@gmx.at>
2020-12-02 14:37:14 +0100

    Add a ClearablePipedInputStream which provides some enhanced functionality
    Provide clearBuffer() and waitAllConsumed() which are useful when sending data
    to audio players

---
+++ src/main/java/org/dstadler/audio/util/ClearablePipedInputStream.java
@@ -0,0 +1,59 @@
+package org.dstadler.audio.util;
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
+/**
+ * An extended {@link PipedInputStream} which supports
+ * clearing the current buffer and waiting for the buffer
+ * to be finished.
+ *
+ * These operations are useful when a PipedInput/OutputStream is
+ * used for sending audio data to the Javva Audio System, see
+ * e.g. the {@link org.dstadler.audio.example.ExamplePlayer} for
+ * how this can be used.
+ */
+public class ClearablePipedInputStream extends PipedInputStream {
+    public ClearablePipedInputStream(PipedOutputStream out, int pipeSize) throws IOException {
+        super(out, pipeSize);
+    }
+
+    /**
+     * Read all bytes that are currently available, i.e.
+     * until available() returns 0.
+     *
+     * This method does not block.
+     *
+     * @throws IOException if an error occurs when calling available() or read()
+     */
+    public synchronized void clearBuffer() throws IOException {
+        // Note: This method is synchronized to avoid possible
+        // blocking read() if another thread tries to read
+        // between calling available() and read() here.
+
+        // read data until none are available any more
+        while (available() > 0) {
+            //noinspection ResultOfMethodCallIgnored
+            read();
+        }
+    }
+
+    /**
+     * Wait until all data is consumed.
+     *
+     * This call will block until another thread has read all bytes
+     * from the buffer.
+     *
+     * @throws IOException  If available() throws an exception
+     * @throws InterruptedException If the thread is interrupted
+     */
+    public void waitAllConsumed() throws IOException, InterruptedException {
+        while (available() > 0) {
+            // tried to use wait() but the piped-stream only notifies readers
+            // when data is becoming available, but we would like to wait
+            // for data to have been consumed
+            Thread.sleep(100);
+        }
+    }
+}
---
+++ src/test/java/org/dstadler/audio/util/ClearablePipedInputStreamTest.java
@@ -0,0 +1,82 @@
+package org.dstadler.audio.util;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.PipedOutputStream;
+
+import static org.dstadler.audio.buffer.Chunk.CHUNK_SIZE;
+import static org.junit.Assert.assertEquals;
+
+public class ClearablePipedInputStreamTest {
+    private final PipedOutputStream out = new PipedOutputStream();
+    private ClearablePipedInputStream in;
+
+    @Before
+    public void setUp() throws IOException {
+         in = new ClearablePipedInputStream(out, 5 * CHUNK_SIZE);
+    }
+
+    @Test
+    public void testWaitAllConsumedNoData() throws IOException, InterruptedException {
+        out.write(0);
+        assertEquals(0, in.read());
+        assertEquals(0, in.available());
+
+        // nothing happens as no data is available anyway
+        in.waitAllConsumed();
+
+        assertEquals(0, in.available());
+    }
+
+    @Test
+    public void testWaitAllConsumedWithData() throws IOException, InterruptedException {
+        out.write(0);
+        assertEquals(1, in.available());
+
+        Thread th = new Thread("Consumer") {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(100);
+
+                    assertEquals(0, in.read());
+                } catch (InterruptedException | IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+        th.start();
+
+        // after the thread consumes the byte, we can continue here
+        in.waitAllConsumed();
+
+        th.join();
+
+        assertEquals(0, in.available());
+    }
+
+    @Test
+    public void testClearNoData() throws IOException {
+        out.write(0);
+        assertEquals(0, in.read());
+        assertEquals(0, in.available());
+
+        // nothing happens as no data is available anyway
+        in.clearBuffer();
+
+        assertEquals(0, in.available());
+    }
+
+    @Test
+    public void testClearWithData() throws IOException {
+        out.write(0);
+        assertEquals(1, in.available());
+
+        // nothing happens as no data is available anyway
+        in.clearBuffer();
+
+        assertEquals(0, in.available());
+    }
+}
