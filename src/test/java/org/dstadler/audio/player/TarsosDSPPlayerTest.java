f91cc5331f88454da159f9a70e0c816a0ba2f226
Dominik Stadler <dominik.stadler@gmx.at>
2021-04-05 22:15:14 +0200

    Add a few more tests which verify how different file formats and how support for mark/reset is handled
    An unexpected file-format currently can lead to "mark/reset not supported" :(

--- src/test/java/org/dstadler/audio/player/TarsosDSPPlayerTest.java
+++ src/test/java/org/dstadler/audio/player/TarsosDSPPlayerTest.java
@@ -4,15 +4,22 @@ import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.junit.Assume;
 import org.junit.Test;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+
 public class TarsosDSPPlayerTest {
     private static final File SAMPLE_FILE = new File("src/test/resources/1-second-of-silence.mp3");
+    private static final File BIN_FILE = new File("src/test/resources/test.bin");
 
     @Test
     public void testSetOption() throws IOException {
@@ -52,6 +59,31 @@ public class TarsosDSPPlayerTest {
     public void testException() throws IOException {
         try (TarsosDSPPlayer player = new TarsosDSPPlayer(new ByteArrayInputStream("test".getBytes()))) {
             player.setOptions("-0.1");
+        }
+    }
+
+    @Test
+    public void testGetAudioInputStreamMP3() throws IOException, UnsupportedAudioFileException {
+        try (InputStream stream = new FileInputStream(SAMPLE_FILE)) {
+            try (AudioInputStream audioStream = AudioSystem.getAudioInputStream(stream)) {
+                assertNotNull(audioStream);
+            }
+        }
+    }
+
+    @Test
+    public void testGetAudioInputStreamBIN() throws IOException {
+        try (InputStream stream = new FileInputStream(BIN_FILE)) {
+            // fails because of "mark/reset not supported", the Java Audio System
+            // relies on it to be available for some of the supported audio-formats!
+            assertThrows(IOException.class, () -> AudioSystem.getAudioInputStream(stream));
+        }
+    }
+
+    @Test
+    public void testGetAudioInputStreamMarkSupported() throws IOException {
+        try (InputStream stream = new BufferedInputStream(new FileInputStream(BIN_FILE), 4096)) {
+            assertThrows(UnsupportedAudioFileException.class, () -> AudioSystem.getAudioInputStream(stream));
         }
     }
 }
