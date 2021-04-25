786b26479f6a3a80a0bd28b56753e190b44d3999
Dominik Stadler <dominik.stadler@gmx.at>
2021-04-25 16:14:22 +0200

    Add a very simple audio player which plays local files
    No separate threads are started, so no seeking/buffering/... is done

---
+++ src/main/java/org/dstadler/audio/example/SimplePlayer.java
@@ -0,0 +1,65 @@
+package org.dstadler.audio.example;
+
+import org.dstadler.audio.player.AudioPlayer;
+import org.dstadler.audio.player.AudioSPIPlayer;
+import org.dstadler.commons.logging.jdk.LoggerFactory;
+import org.dstadler.commons.util.SuppressForbidden;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static org.dstadler.audio.buffer.Chunk.CHUNK_SIZE;
+
+/**
+ * A very simple commandline audio player to show usage
+ * of some of the components that are contained in this project.
+ *
+ * Here you can only specify a local filename and seek is not possible,
+ * look at {@link ExamplePlayer} for a slightly more advanced example.
+ */
+public class SimplePlayer {
+    private final static Logger log = LoggerFactory.make();
+
+    @SuppressForbidden(reason = "Uses System.exit()")
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.err.println("Usage: SimplePlayer <file>");
+            System.exit(1);
+        }
+
+        LoggerFactory.initLogging();
+
+        run(args[0]);
+    }
+
+    private static void run(String file) throws IOException {
+        log.info("Playing file " + file);
+
+        final InputStream input = new BufferedInputStream(new FileInputStream(file), CHUNK_SIZE);
+
+        try {
+            AudioPlayer player = createPlayer(input);
+
+            //player.setOptions("");
+
+            player.play();
+        } catch (Throwable e) {
+            log.log(Level.WARNING, "Caught unexpected exception", e);
+        }
+
+        log.info("Waiting for player to stop");
+    }
+
+    private static AudioPlayer createPlayer(InputStream inputStream) throws IOException {
+        // any of the implementations will play .mp3 streams
+        // the SPI-based one can play OggVorbis as well
+
+//        return new TarsosDSPPlayer(inputStream);
+        return new AudioSPIPlayer(inputStream);
+//        return new JLayerPlayer(inputStream);
+    }
+}
